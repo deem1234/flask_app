7d0307a23605ca8596731b2c57ed16a452dd8b01
Tariq <>
2018-04-08 14:50:33 -0500

    foruth

--- application.py
+++ application.py
@@ -59,7 +59,8 @@ def hello_world():
 # 
 # run the app.
 if __name__ == '__main__':
-    app.run()
+    app.debug = True
+    app.run(host='0.0.0.0',port=80)
 #     
 # if __name__ == "__main__":
 #     # Setting debug to True enables debug output. This line should be
