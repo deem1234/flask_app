7d43ffebf0f7f0e23fe6ef3a36f2397aa45322d1
Tariq <>
2018-04-11 11:14:41 -0500

    exam day 1

--- application.py
+++ application.py
@@ -64,5 +64,6 @@ if __name__ == '__main__':
 # if __name__ == "__main__":
 #     # Setting debug to True enables debug output. This line should be
 #     # removed before deploying a production app.
-    app.debug = True
-    app.run(host='0.0.0.0',port=80)
+    app.run()
+#     app.debug = True
+#     app.run(host='0.0.0.0',port=80)
